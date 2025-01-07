2d18fa0e325a9e64cea45073deeaff1b92573828
Pawel <37085168+xarels@users.noreply.github.com>
2025-01-07 11:46:17 +0000

    test

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST='{"node_modules\\\\next\\\\dist\\\\client\\\\index.js -> ../pages/_app":{"id":5203,"files":["static/chunks/203.2b4c1ee4fbe3a7cf.js"]},"node_modules\\\\next\\\\dist\\\\client\\\\index.js -> ../pages/_error":{"id":6218,"files":["static/chunks/218.57a830a2c55ba802.js"]}}';
