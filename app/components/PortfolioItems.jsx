e63cc974d42ea9e8e6b412497f53e762dbf5b989
Pawel <37085168+xarels@users.noreply.github.com>
2025-01-10 23:10:08 +0000

    warning fix

--- app/components/PortfolioItems.jsx
+++ app/components/PortfolioItems.jsx
@@ -4,21 +4,15 @@ import imageData from "../data/imageData.json"
 
 const PortfolioItems = () => {
   return (
-    <>
         <div id="portfolio-items" className="grid">
             {imageData.map ((image) => (
             <div key={image.id} className='portfolio-item'>
-            <img 
-            src={image.image}
-            alt={image.title}
-            className='image-container'
-            />                
+            <img src={image.image} alt={image.title} className='image-container' />                
             <h2>{image.title}</h2>   
             <p>{image.description}</p>    
             </div>
             ))}
         </div>
-    </>
   );
 }
 
