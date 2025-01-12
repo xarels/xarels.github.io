74fff1e3c572244697ea8d0fdefb42fe108491b8
Pawel <37085168+xarels@users.noreply.github.com>
2025-01-12 22:36:04 +0000

    fix

---
+++ app/pages/Home.jsx
@@ -0,0 +1,45 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import Hero from './Hero';
+import Main from './Main';
+import Footer from "../components/Footer";
+
+export const Home = () => {
+
+  const [scrolled, setScrolled] = useState(false);
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    // Avoid hydration mismatch by delaying the effect until after the component mounts
+    setHasMounted(true);
+
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight / 2) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  // Ensure rendering only after mounting
+  if (!hasMounted) {
+    return null; // Render nothing until the client is ready
+  }
+
+  return (
+    <div className={scrolled ? 'scrolled' : 'notScrolled'}>
+      <header >
+        <Hero />
+      </header>
+      <Main />
+    <footer>
+      <Footer />
+    </footer>
+    </div>
+  );
+};
+export default Home;
